912be80ffd67757ea6d482d80894d79c0c511938
kadir-zeren <k4dzeren@gmail.com>
2024-03-06 17:58:04 +0300

    push

--- app.js
+++ app.js
@@ -52,3 +52,70 @@
 // colors[100] = "Pink";
 
 // console.log(colors);
+
+// let a = 5;
+// let b = a;
+// console.log(a);
+// console.log(b);
+
+// a = 6;
+// console.log(b);
+
+// b = a;
+// console.log(b);
+
+// let arr1 = [1, 2, "Three", false];
+
+// const arr2 = arr1;
+// console.log(arr1);
+// console.log(arr2);
+
+// arr1[0] = 0;
+
+// console.log(arr1);
+// console.log(arr2);
+
+// const c = ["2"];
+// console.log(c);
+
+// console.log(Boolean(c));
+
+// const d = ["2"];
+// console.log(c == d);
+
+// console.log(arr1 == arr2);
+
+// const colors = ["Red", "Green", "Blue", "Yellow", "Brown"];
+
+// const cls = colors.slice();
+// console.log(colors);
+// console.log(cls);
+
+// colors[0] = "Black";
+// console.log(colors);
+// console.log(cls);
+
+// console.log(colors.slice(2, 4));
+// console.log(colors.slice(-2));
+// console.log(colors.slice(2));
+// console.log(colors.slice(8));
+// console.log(colors.slice(-2, 1));
+// console.log(colors.slice(-3, -1));
+
+const colors = ["Red", "Green"];
+console.log(colors.length);
+
+let c = "White";
+
+colors.push("Blue", 2023, c);
+colors.push(2023);
+
+console.log(colors.length);
+
+const car = ["Audi", "BMW"];
+const car2 = ["Ford", "Tesla", "Mercedes"];
+
+car.push(car2);
+car.push(...car2);
+
+console.log(car[2][2]);
